20b748d3785eb682c4233979bf40532858d0c6c3
Gurpreet Singh <singhgurpreet1212@gmail.com>
2021-03-17 16:46:13 +0530

    Show a single stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,6 +1,26 @@
-import React from "react";
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
+
+const StreamShow = (props) => {
+  useEffect(() => {
+    props.fetchStream(props.match.params.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!props.stream) return <div>Loading...</div>;
+
+  const { title, description } = props.stream;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
